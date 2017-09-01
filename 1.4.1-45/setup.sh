63e4f99b0a037ee7036e50701554491d47fa39c3
Sambit Mishra <samis@microsoft.com>
2017-09-01 13:00:45 -0700

    Testing 1.4.1-45

---
+++ 1.4.1-45/setup.sh
@@ -0,0 +1,16 @@
+TMPDIR="/opt"
+cd $TMPDIR
+wget https://keiko.blob.core.windows.net/container/omsagent-1.4.1-45.universal.x64.sh 
+chmod 775 $TMPDIR/*.sh
+
+$TMPDIR/omsagent-*.universal.x64.sh --extract
+mv $TMPDIR/omsbundle* $TMPDIR/omsbundle
+$TMPDIR/omsbundle/bundles/scx-1.6.*-*.universal.x64.sh --install
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsagent*.deb
+/usr/bin/dpkg -i $TMPDIR/omsbundle/100/omsconfig*.deb
+/$TMPDIR/omsbundle/oss-kits/docker-cimprov-1.0.0-*.x86_64.sh --install
+#Use downloaded docker-provider instead of the bundled one
+#/$TMPDIR/docker-cimprov-1.0.0-*.x86_64.sh --install
+rm -rf $TMPDIR/omsbundle
+rm -f $TMPDIR/omsagent*.sh
+rm -f $TMPDIR/docker-cimprov*.sh
